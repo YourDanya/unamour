fc48fa8f8987fb6ce0a9f1d60d9d0f378137dd20
Danyl <pochtaformumumu@gmail.com>
2022-08-20 23:17:24 +0300

    updating shop-item-preview-component, creating load-image component

---
+++ components/common/load-image/load-image.component.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+type loadImageProps = {
+
+}
+
+const LoadImage: React.FC<loadImageProps> = () => {
+    return (
+        <div className={'loadImage'}>
+        </div>
+    )
+}
+
+export default LoadImage
