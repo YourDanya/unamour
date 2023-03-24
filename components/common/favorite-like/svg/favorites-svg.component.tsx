6a5c7ffc99c515d91ad66fb4f59bce5c2afc31f4
Danyl <pochtaformumumu@gmail.com>
2023-03-24 03:44:08 +0200

    updating favorites svg

--- components/common/favorite-like/svg/favorites-svg.component.tsx
+++ components/common/favorite-like/svg/favorites-svg.component.tsx
@@ -1,16 +1,16 @@
 const FavoritesSvg = () => {
     return (
         <svg xmlns="http://www.w3.org/2000/svg" width="14" height="18" viewBox='0 0 14 18' fill="none" display='block'>
-            <g filter="url(#a)">
                 <path fill="#BDBDBD" fillRule="evenodd" d="M.5 0h13v17.482l-6.487-4.364L.5 17.482V0Z"
                       clipRule="evenodd"/>
-            </g>
         </svg>
     )
 }
 
 export default FavoritesSvg
 
+{/*<g filter="url(#a)">*/}
+{/*</g>*/}
 
 // <defs>
 // <filter id="a" width="21" height="25.482" x="-3.5" y="-4" colorInterpolationFilters="sRGB"
