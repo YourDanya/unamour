784936c45d72747c806018e6370257a2e0955e01
Danyl <pochtaformumumu@gmail.com>
2023-03-24 02:57:24 +0200

    trying to improve favorites svg invisibility in safari

--- components/common/favorite-like/svg/favorites-svg.component.tsx
+++ components/common/favorite-like/svg/favorites-svg.component.tsx
@@ -5,15 +5,15 @@ const FavoritesSvg = () => {
                 <path fill="#BDBDBD" fillRule="evenodd" d="M.5 0h13v17.482l-6.487-4.364L.5 17.482V0Z"
                       clipRule="evenodd"/>
             </g>
-            <defs>
-                <filter id="a" width="21" height="25.482" x="-3.5" y="-4" colorInterpolationFilters="sRGB"
-                        filterUnits="userSpaceOnUse">
-                    <feFlood floodOpacity="0" result="BackgroundImageFix"/>
-                    <feGaussianBlur in="BackgroundImage" stdDeviation="2"/>
-                    <feComposite in2="SourceAlpha" operator="in" result="effect1_backgroundBlur_10666_4735"/>
-                    <feBlend in="SourceGraphic" in2="effect1_backgroundBlur_10666_4735" result="shape"/>
-                </filter>
-            </defs>
+            {/*<defs>*/}
+            {/*    <filter id="a" width="21" height="25.482" x="-3.5" y="-4" colorInterpolationFilters="sRGB"*/}
+            {/*            filterUnits="userSpaceOnUse">*/}
+            {/*        <feFlood floodOpacity="0" result="BackgroundImageFix"/>*/}
+            {/*        <feGaussianBlur in="BackgroundImage" stdDeviation="2"/>*/}
+            {/*        <feComposite in2="SourceAlpha" operator="in" result="effect1_backgroundBlur_10666_4735"/>*/}
+            {/*        <feBlend in="SourceGraphic" in2="effect1_backgroundBlur_10666_4735" result="shape"/>*/}
+            {/*    </filter>*/}
+            {/*</defs>*/}
         </svg>
     )
 }
