5687d3b3cd5bd93a2b6bc395f5ae8ebd2561afaa
Danyl <pochtaformumumu@gmail.com>
2023-08-25 11:47:41 +0300

    updating slider types

--- app/_common/components/slider/slider.types.ts
+++ app/_common/components/slider/slider.types.ts
@@ -4,3 +4,22 @@ import React from 'react'
 export type EventState = ReturnType<typeof useGetState> & { event: MouseEvent | TouchEvent }
 
 export type ReactEventState = ReturnType<typeof useGetState> & {event: React.MouseEvent | React.TouchEvent}
+
+export type SliderProps = {
+    children: React.ReactNode,
+    current?: number,
+    setCurrent?: (index: number) => void,
+    perSlide?: number,
+    className?: string,
+    infinite?: boolean,
+    container?: boolean
+}
+
+export type MoveRef = {
+    startX: number,
+    moving: boolean,
+    current: number,
+    fast: boolean,
+    clientX: number,
+    blocking: boolean
+}
